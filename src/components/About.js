6d29d832e89a9224597875412df0a99afae432a2
andrean junior <andreanjunior121@gmail.com>
2024-03-28 20:47:49 -0300

    :tada: about

--- src/components/About.js
+++ src/components/About.js
@@ -21,12 +21,12 @@ export default function About(props) {
                     <img className="profile-pic" src={profilepic} alt="Hermione Granger Profile Picture" />
                 </div>
                 <div className="nine columns main-col">
-                    <h2>About Me</h2>
+                    <h2>Sobre mim</h2>
 
                     <p>{bio}</p>
                     <div className="row">
                         <div className="columns contact-details">
-                            <h2>Contact Details</h2>
+                            <h2>Detalhes do contato</h2>
                             <p className="address">
                                 <span>{name}</span><br />
                                 <span>{street}<br />
