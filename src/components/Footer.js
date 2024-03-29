023b64609e4013f65bb5c85e6c0234d2b4370170
andrean junior <andreanjunior121@gmail.com>
2024-03-29 00:15:09 -0300

    :tada:commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,7 +18,7 @@ export default function Footer(props) {
                     </ul>
 
                     <ul className="copyright">
-                        <li>&copy; Copyright 2020 Mírian Silva</li>
+                        <li>&copy; Copyright 2024 Andrean Jr.</li>
                         <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
                     </ul>
 
